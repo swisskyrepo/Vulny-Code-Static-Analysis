626c3581b8a80f93d3e9e35854d5e24ad2d6d003
Swissky <maxropelewski@gmail.com>
2017-06-06 22:51:01 +0200

    BUGFIX - Colored variable badly displayed when count>2

--- functions.py
+++ functions.py
@@ -5,30 +5,15 @@ import re
 from indicators import *
 
 # Replace the nth occurence of a string
-# From https://stackoverflow.com/questions/35091557/replace-nth-occurrence-of-substring-in-string
-def nth_replace(string, old, new, n=1, option='only nth'):
-    """
-    This function replaces occurrences of string 'old' with string 'new'.
-    There are three types of replacement of string 'old':
-    1) 'only nth' replaces only nth occurrence (default).
-    2) 'all left' replaces nth occurrence and all occurrences to the left.
-    3) 'all right' replaces nth occurrence and all occurrences to the right.
-    """
-    if option == 'only nth':
+# Inspired from https://stackoverflow.com/questions/35091557/replace-nth-occurrence-of-substring-in-string
+def nth_replace(string, old, new, n):
+    if string.count(old) >= n:
         left_join = old
         right_join = old
-    elif option == 'all left':
-        left_join = new
-        right_join = old
-    elif option == 'all right':
-        left_join = old
-        right_join = new
-    else:
-        print("Invalid option. Please choose from: 'only nth' (default), 'all left' or 'all right'")
-        return None
-    groups = string.split(old)
-    nth_split = [left_join.join(groups[:n]), right_join.join(groups[n:])]
-    return new.join(nth_split)
+        groups = string.split(old)
+        nth_split = [left_join.join(groups[:n]), right_join.join(groups[n:])]
+        return new.join(nth_split)
+    return string.replace(old, new)
 
 
 # Display the found vulnerability with basic informations like the line
